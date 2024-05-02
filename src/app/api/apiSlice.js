1501adcf8b19ee922f1d979842ac273c977c5945
Bruce NKUNDABAGENZI <nbruce420@gmail.com>
2024-05-02 19:55:58 +0200

    Redeployed

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { setCredentials } from "../../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: "https://technotes-service-fdra.onrender.com",
+    baseUrl: "https://technotes-services-fdra.onrender.com",
     credentials: "include",
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token;
